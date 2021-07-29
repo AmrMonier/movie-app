9c40fc7e64bbefdb5b3f868c980fa91dfc8c6399
Amr Monier <a.monier2107@gmail.com>
2021-07-29 23:48:12 +0200

    removed the authenticated middleware from movies routes

--- Routes/MoviesRoutes.js
+++ Routes/MoviesRoutes.js
@@ -6,14 +6,14 @@ const Router = express.Router
 const routes = new Router();
 
 // Add routes
-routes.get('/popular',middlewares.validateUser, MoviesController.popularMovies)
-routes.get('/top-rated',middlewares.validateUser, MoviesController.topRated)
-routes.get('/most-recent',middlewares.validateUser, MoviesController.mostRecent)
-routes.get('/:movie_id',middlewares.validateUser, MoviesController.getMovie)
-routes.post('/:movie_id/rate',middlewares.validateUser, MoviesController.reviewMovie)
-routes.get('/:movie_id/credits',middlewares.validateUser, MoviesController.movieCredits)
-routes.get('/:movie_id/images',middlewares.validateUser, MoviesController.movieImages)
-routes.get('/:movie_id/videos',middlewares.validateUser, MoviesController.movieVideos)
+routes.get('/popular', MoviesController.popularMovies)
+routes.get('/top-rated', MoviesController.topRated)
+routes.get('/most-recent', MoviesController.mostRecent)
+routes.get('/:movie_id', MoviesController.getMovie)
+routes.post('/:movie_id/rate', middlewares.validateUser, MoviesController.reviewMovie)
+routes.get('/:movie_id/credits', MoviesController.movieCredits)
+routes.get('/:movie_id/images', MoviesController.movieImages)
+routes.get('/:movie_id/videos', MoviesController.movieVideos)
 
 
 export default routes;
