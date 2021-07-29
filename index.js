3626e7d31256675b4cc8399313b38febfdeab618
Amr Monier <a.monier2107@gmail.com>
2021-07-29 23:49:26 +0200

    added both Cors and Morgan

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import cors from "cors"
+import morgan from "morgan"
 dotenv.config()
 
 import authenticationRoutes from './Routes/AuthenticationRoutes.js'
 import movieRoutes from './Routes/MoviesRoutes.js'
 
 const app = express()
+app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 const port = process.env.PORT || 8080
 app.use('/api/auth', authenticationRoutes)
-app.use('/api/movie', movieRoutes)
+app.use('/api/movies', movieRoutes)
 
 app.use(express.json())
 app.listen(port, () => {
